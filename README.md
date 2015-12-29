63c8940d87cd244e19c9147d7013b8c6f1765fe9
Ian Kloo <ian.p.kloo@BELVWK1839CA514.nae.ds.army.mil>
2015-12-29 14:08:59 -0500

    Added hour, minute, and second

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 Fills in missing time series values in R.
 
 The function currently only works on the year, month, and day levels.  I plan to add functionality down to the second level (but need to find some suitable test data).
+
+## Purpose of the Function
+When performing time series analysis, it is important to make sure every time period is represented.  For example, if you select a minute as your time granualrity, every minute in your date range of interest must be represented in the data.  Consider a real world example: if you are looking at homicides over time for a small city with a day granularity, you will likely have many observations over a year, but you will (hopefully) not have a homicide every day.  It is a common mistake to analyze/visualize data without filling in every time period.
+
+I found myself often trying to fill in these gaps in time series data in R, so wrote this function to help.
+
+## Using the Function
+The data for the function must be a dataframe with at least a date column.  This column must be able to be coerced into a date object using the as.POSIXct() function.  If you think this could be an issue, try converting your date using as.POSIXct().  If you find that you need to provide the as.POSIXct() function a specific format, please convert your dates before running the dateFill() function.
+
+With no other options specified, the function will assume there is a row for every "event" in the time series.  For example, if you are building a time series of crimes, every row will have a date that represents when a single crime is committed.  The function is built to aggregate events into the specified time granularity (day, month, year, etc.), then fill in any missing holes.  
+
+If your data is already aggregated (i.e. a row represents a time period with some count of events that occured in that time period), you will need to specify the column name where the counts are held.  This will cause the function to do any further aggregation by summing this column instead of counting a row as a single event.   
--- timeseries function.R
+++ timeseries function.R
@@ -1,30 +1,83 @@
 
 ##Function to fill in holes in time series data
-
-dateFill <- function(data, aggLevel = c('year','month','day')){
+dateFill <- function(data, aggLevel = c('year','month','day'), sumvar= 'none'){
+  require(stringr)
   cnames <- colnames(data)
-  data$Date <- as.Date(data$Date)
-  data$Year <- as.numeric(format(data$Date,'%Y'))
-  data$Month <- as.numeric(format(data$Date, '%m'))
-  data$Day <- as.numeric(format(data$Date, '%d'))
+  data$Date <- as.POSIXct(data$Date)
   data$Count <- rep.int(1,nrow(data))
-  
   if(aggLevel=='year'){
-    data.sub <- aggregate(Count ~ Year, data, FUN = sum)
+    data$Year <- as.numeric(format(data$Date,'%Y'))
+    if(sumvar == 'none'){
+      data.sub <- aggregate(Count ~ Year, data, FUN = sum)
+    } else{
+      data.sub <- aggregate(eval(parse(text=sumvar)) ~ Year, data, FUN=sum)
+      colnames(data.sub)[2] <- 'Sum'
+    }
     full.time <- data.frame(Year=as.numeric(format(seq(min(data$Date), max(data$Date), by='year'),'%Y')))
     merged.data <- merge(full.time, data.sub, by='Year',all=TRUE)
     merged.data[is.na(merged.data)] <- 0
   } else if(aggLevel=='month'){
-    data$yearmonth <- as.Date(as.yearmon(paste(data$Year, data$Month),'%Y %m'))
-    data.sub <- aggregate(Count ~ yearmonth, data, FUN = sum)
-    full.time <- data.frame(yearmonth=seq(min(data$yearmonth), max(data$yearmonth), by='month'))
-    merged.data <- merge(full.time, data.sub, by='yearmonth',all=TRUE)
+    data$Month <- paste(str_sub(data$Date, start=1, end=7),'-01',sep='')
+    if(sumvar == 'none'){
+      data.sub <- aggregate(Count ~ Month, data, FUN = sum)
+    } else{
+      data.sub <- aggregate(eval(parse(text=sumvar)) ~ Month, data, FUN=sum)
+      colnames(data.sub)[2] <- 'Sum'
+    }
+    data.sub$Month <- as.POSIXct(data.sub$Month)
+    data$Month <- as.POSIXct(data$Month)
+    full.time <- data.frame(Month=seq(min(data$Month), max(data$Month), by='month'))
+    merged.data <- merge(full.time, data.sub, by='Month',all=TRUE)
     merged.data[is.na(merged.data)] <- 0
-    warning('When aggregating data to month scale, all dates are set to the first day of the month')
   } else if(aggLevel=='day'){
-    data.sub <- aggregate(Count ~ Date, data, FUN = sum)
-    full.time <- data.frame(Date=seq(min(data$Date), max(data$Date), by='day'))
-    merged.data <- merge(full.time, data.sub, by='Date',all=TRUE)
+    data$Day <- as.Date(data$Date)
+    if(sumvar == 'none'){
+      data.sub <- aggregate(Count ~ Day, data, FUN = sum)
+    } else{
+      data.sub <- aggregate(eval(parse(text=sumvar)) ~ Day, data, FUN=sum)
+      colnames(data.sub)[2] <- 'Sum'
+    }
+    full.time <- data.frame(Day=seq(min(data$Day), max(data$Day), by='day'))
+    merged.data <- merge(full.time, data.sub, by='Day',all=TRUE)
+    merged.data[is.na(merged.data)] <- 0
+  } else if(aggLevel=='hour'){
+    data$Hour <- paste(str_sub(data$Date, start=1, end=14),'00:00',sep='')
+    if(sumvar == 'none'){
+      data.sub <- aggregate(Count ~ Hour, data, FUN = sum)
+    } else{
+      data.sub <- aggregate(eval(parse(text=sumvar)) ~ Hour, data, FUN=sum)
+      colnames(data.sub)[2] <- 'Sum'
+    }
+    data.sub$Hour <- as.POSIXct(data.sub$Hour)
+    data$Hour <- as.POSIXct(data$Hour)
+    full.time <- data.frame(Hour=seq(min(data$Hour), max(data$Hour), by='hour'))
+    merged.data <- merge(full.time, data.sub, by='Hour',all=TRUE)
+    merged.data[is.na(merged.data)] <- 0
+  } else if(aggLevel=='minute'){
+    data$Minute <- paste(str_sub(data$Date, start=1, end=17),'00',sep='')
+    if(sumvar == 'none'){
+      data.sub <- aggregate(Count ~ Minute, data, FUN = sum)
+    } else{
+      data.sub <- aggregate(eval(parse(text=sumvar)) ~ Minute, data, FUN=sum)
+      colnames(data.sub)[2] <- 'Sum'
+    }
+    data.sub$Minute <- as.POSIXct(data.sub$Minute)
+    data$Minute <- as.POSIXct(data$Minute)
+    full.time <- data.frame(Minute=seq(min(data$Minute), max(data$Minute), by='min'))
+    merged.data <- merge(full.time, data.sub, by='Minute', all=TRUE)
+    merged.data[is.na(merged.data)] <- 0
+  } else if(aggLevel=='second'){
+    data$Second <- str_sub(data$Date, start=1, end=19)
+    if(sumvar == 'none'){
+      data.sub <- aggregate(Count ~ Second, data, FUN = sum)
+    } else{
+      data.sub <- aggregate(eval(parse(text=sumvar)) ~ Second, data, FUN=sum)
+      colnames(data.sub)[2] <- 'Sum'
+    }
+    data.sub$Second <- as.POSIXct(data.sub$Second)
+    data$Second <- as.POSIXct(data$Second)
+    full.time <- data.frame(Second=seq(min(data$Second), max(data$Second), by='sec'))
+    merged.data <- merge(full.time, data.sub, by='Second',all=TRUE)
     merged.data[is.na(merged.data)] <- 0
   }
   dataFull <- merged.data
@@ -32,10 +85,21 @@ dateFill <- function(data, aggLevel = c('year','month','day')){
   return(dataFull)
 }
 
+
+
+
 ###Testing
-#df <- read.csv('ChicagoHomicides.csv')
+#df <- read.csv('Chiraq_Subset.csv')
 #df <- subset(df,select=c('Date','Arrest'))
 #df <- df[df$Arrest==TRUE,]
 #df$Arrest <- as.numeric(df$Arrest)
 
-#newdata <- dateFill(df,aggLevel='year')
+#df$Date <- as.POSIXct(df$Date, format='%m/%d/%y')
+#dateFill(df,'month')
+
+
+#df$Num <- round(rnorm(nrow(df),10,2))
+#df$Arrest <- NULL
+
+#dateFill(df,'month','Num')
+
